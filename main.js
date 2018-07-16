8790e1896061d1a3afe22fbb2f2121c6b33d418f
MAREK-PC\Marek <marek.kawa27@gmail.com>
2018-07-16 20:49:06 +0200

    Remove main.js file

--- main.js
+++
@@ -1,5 +0,0 @@
-slider(".bg", 3000, [
-  'https://cdn.mkava.pl/img/background-slideshow/1.jpg', 
-  'https://cdn.mkava.pl/img/background-slideshow/2.jpeg',
-  'https://cdn.mkava.pl/img/background-slideshow/2.jpeg'
-  ]);

776012789cd9b74bfbbf8f7ba5d9a57bfe37eae8
MAREK-PC\Marek <marek.kawa27@gmail.com>
2018-07-16 20:12:56 +0200

    Change img url

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 slider(".bg", 3000, [
-  'https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 
-  'https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  'https://cdn.mkava.pl/img/background-slideshow/1.jpg', 
+  'https://cdn.mkava.pl/img/background-slideshow/2.jpeg',
+  'https://cdn.mkava.pl/img/background-slideshow/2.jpeg'
   ]);
